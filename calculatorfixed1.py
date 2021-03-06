9b1f959c6037538fb78b273bddd9f20bcc54ef07
Treenutgod <78819516+Treenutgod@users.noreply.github.com>
2021-03-05 23:55:59 -0500

    Update calculatorfixed1.py

--- calculatorfixed1.py
+++ calculatorfixed1.py
@@ -2,19 +2,16 @@ def math_caluacaltions():
     num1 = float(input("Enter first number: "))
     operator = input("give me a operation")
     num2 = float(input("Enter second number: "))
-    valid = ["-", "+", "/", "", "**"]
+    valid = {
+        "-": f"{num1-num2}",
+        "+": f"{num1+num2}",
+        "/": f"{num1/num2}",
+        "": f"{num1}{num2}",
+        "**": f"{num1**num2}"
+    }
     if operator in valid:
-        math_method=valid.index(operator)
-        if math_method==0:
-            print(f"The answer to {num1}{operator}{num2} is {num1-num2}")
-        if math_method==1:
-            print(f"The answer to {num1}{operator}{num2} is {num1+num2}")
-        if math_method==2:
-            print(f"The answer to {num1}{operator}{num2} is {num1/num2}")
-        if math_method==3:
-            print(f"The answer to {num1}{operator}{num2} is {num1num2}")
-        if math_method==4:
-            print(f"The answer to {num1}{operator}{num2} is {num1**num2}")
+        final_answer = f"The answer to {num1}{operator}{num2} is " + valid[operator]
+        print(final_answer)
     else:
         print(A+B)
 def Math_answer():
@@ -24,8 +21,6 @@ def Math_answer():
             end=str(input("\n would you want to end the program 1=Yes anything=no"))
             if end=="1":
                 break
-            elif end!="1":
-                pass
         except:
             print("Sorry, but excepetion has happend. Please make sure that it is a logcial statment and only numbers and valid symbols are put in.")
             break
